cece12308c810b69d41819e09417277980ed4f94
alex__rogers <alex.rogers@cs.ox.ac.uk>
2020-06-05 15:14:43 +0100

    Add temperature measurement white paper.

---
+++ temperature.py
@@ -0,0 +1,53 @@
+import re
+import csv
+
+from os import listdir
+from os.path import isfile, join
+from datetime import datetime, timezone, timedelta
+
+directory = "."
+
+COMMENT_START = 0x38
+COMMENT_LENGTH = 0x180
+
+files = [f for f in listdir(directory) if isfile(join(directory, f)) and ".WAV" in f]
+
+with open("comments.csv", "w", newline="") as csvfile:
+
+  csvWriter = csv.writer(csvfile, delimiter=",")
+    
+  csvWriter.writerow(["Index", "File", "Time", "Battery (V)", "Temperature (C)", "Comment"])
+    
+  for i, fi in enumerate(sorted(files)):
+        
+    with open(join(directory, fi), "rb") as f:
+        
+      # Read the comment out of the input file
+
+      f.seek(COMMENT_START)
+
+      comment = f.read(COMMENT_LENGTH).decode("ascii").rstrip("\0")
+
+      # Read the time and timezone from the header
+
+      ts = re.search(r"Recorded at (\d\d:\d\d:\d\d \d\d/\d\d/\d\d\d\d)", comment)[1]
+
+      tz = re.search(r"\(UTC([-|+]\d+)?:?(\d\d)?\)", comment)
+
+      hrs = 0 if tz[1] is None else int(tz[1])
+
+      mins = 0 if tz[2] is None else -int(tz[2]) if hrs < 0 else int(tz[2])
+
+      timestamp = datetime.strptime(ts, "%H:%M:%S %d/%m/%Y")
+        
+      timestamp = timestamp.replace(tzinfo=timezone(timedelta(hours=hrs, minutes=mins)))
+
+      # Read the battery voltage and temperature from the header
+
+      battery = re.search(r"battery state was (\d.\d)V", comment)[1]
+
+      temperature = re.search(r"temperature was (-?\d+.\d)C", comment)[1]
+
+      # Print the output row
+    
+      csvWriter.writerow([i, fi, timestamp.isoformat(), battery, temperature, comment])
